255706105c297f52a2f167b37a4b75baa118e2b3
Joeri Abbo <joeriabbo@hotmail.com>
2023-04-08 22:41:34 +0200

    Add tailwind.

--- pages/about.tsx
+++ pages/about.tsx
@@ -2,13 +2,13 @@ import Link from 'next/link'
 import Layout from '../components/Layout'
 
 const AboutPage = () => (
-  <Layout title="About | Next.js + TypeScript Example">
-    <h1>About</h1>
-    <p>This is the about page</p>
-    <p>
-      <Link href="/">Go home</Link>
-    </p>
-  </Layout>
+    <Layout title="About | Next.js + TypeScript Example">
+        <h1>About</h1>
+        <p>This is the about page</p>
+        <p>
+            <Link href="/">Go home</Link>
+        </p>
+    </Layout>
 )
 
 export default AboutPage
